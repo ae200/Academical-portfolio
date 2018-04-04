d00b6297b8d938929531495d88a00703df2f6df2
Adegbenga Efunnuga <festlove2007@yahoo.com>
2018-04-04 10:18:53 +0100

    Built pages via controller generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/managers'
+
+  get 'pages/advertise'
+
+  get 'pages/event'
+
+  get 'pages/library'
+
+  get 'pages/contact'
+
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
