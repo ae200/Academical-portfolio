9fee1d44fdd902ff5997c6219dd7b8d558c9e113
Adegbenga Efunnuga <festlove2007@yahoo.com>
2018-04-04 12:46:52 +0100

    Added portfolio and categories items via Resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :portfolios
   get 'pages/home'
 
   get 'pages/about'
@@ -12,7 +13,18 @@ Rails.application.routes.draw do
   get 'pages/library'
 
   get 'pages/contact'
+  
+  get 'pages/trainers'
+  
+  get 'pages/equipmentproviders'
+  
+  get 'pages/eventmanagers'
+  
+  get 'pages/facilitators'
+  
+  
 
   resources :posts
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404100401) do
+ActiveRecord::Schema.define(version: 20180404112920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.string "contact"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
