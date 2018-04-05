6981f9e6ebbe522f06b31d53f6a424b7c980d787
Adegbenga Efunnuga <festlove2007@yahoo.com>
2018-04-05 02:02:21 +0100

    Updated status for enum

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
      belongs_to :category
+	 enum status: {draft: 0, published: 1 }
 	 
 	extend FriendlyId
     friendly_id :theme, use: :slugged
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404232918) do
+ActiveRecord::Schema.define(version: 20180405004127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20180404232918) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
