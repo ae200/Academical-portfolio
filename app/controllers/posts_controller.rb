6e38e1217aeed2223a88600246fd7b6c4274c7ea
Adegbenga Efunnuga <festlove2007@yahoo.com>
2018-04-12 03:44:36 +0100

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "post" 
-   
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all 
   # GET /posts
   # GET /posts.json
   def index
